1e83f48fc7aeeb3146f80054b53e60af3c42810f
scott <mic1028002@gmail.com>
2015-01-17 00:47:55 +0800

    update view script

--- app/scripts/controllers/editor.js
+++ app/scripts/controllers/editor.js
@@ -9,7 +9,20 @@ angular.module('myAngularJsStudyApp')
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
     circle.x = 100;
     circle.y = 100;
+
+
+    //
+    circle.addEventListener('click', function(e){
+      alert(1);
+    });
+
+
+    // add to scene
     $scope.stage.addChild(circle);
 
-    $scope.stage.update();
+
+    // update canvas
+    createjs.Ticker.addEventListener("tick", function(){
+      $scope.stage.update();
+    });
   });
